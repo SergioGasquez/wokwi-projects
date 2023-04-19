1f38a1ce8148ac403c856337e404305082688b46
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-04-19 15:02:26 +0200

    style: 🎨 Format hal name

--- esp32-no_std-template/main.rs
+++ esp32-no_std-template/main.rs
@@ -1,11 +1,9 @@
 #![no_std]
 #![no_main]
 
-use esp32_hal::{
-    clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc,
-};
 use esp_backtrace as _;
 use esp_println::println;
+use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc};
 
 #[entry]
 fn main() -> ! {
