8fcf8c8c9fe55015c9d9a8e0877421d3f3335c9d
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-04-19 15:01:55 +0200

    feat: ✨ Update esp32c3 nostd projects

--- esp32c3-no_std-blinky/main.rs
+++ esp32c3-no_std-blinky/main.rs
@@ -1,17 +1,21 @@
+//! Blinks an LED
+//!
+//! This assumes that a LED is connected to the pin assigned to `led`. (GPIO4)
+
 #![no_std]
 #![no_main]
 
-use esp32c3_hal::{
-    clock::ClockControl, pac::Peripherals, prelude::*, timer::TimerGroup, Delay, Rtc, IO,
-};
 use esp_backtrace as _;
 use esp_println::println;
-use riscv_rt::entry;
+use hal::{
+    clock::ClockControl, gpio::IO, peripherals::Peripherals, prelude::*, timer::TimerGroup, Delay,
+    Rtc,
+};
 
 #[entry]
 fn main() -> ! {
-    let peripherals = Peripherals::take().unwrap();
-    let system = peripherals.SYSTEM.split();
+    let peripherals = Peripherals::take();
+    let mut system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
     // Disable the RTC and TIMG watchdog timers
@@ -20,20 +24,23 @@ fn main() -> ! {
     let mut wdt0 = timer_group0.wdt;
     let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
     let mut wdt1 = timer_group1.wdt;
-
-    rtc.swd.disable();
     rtc.rwdt.disable();
     wdt0.disable();
     wdt1.disable();
+    println!("Hello world!");
 
-    let mut delay = Delay::new(&clocks);
+    // Set GPIO4 as an output, and set its state high initially.
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
     let mut led = io.pins.gpio4.into_push_pull_output();
+
     led.set_high().unwrap();
 
+    // Initialize the Delay peripheral, and use it to toggle the LED state in a
+    // loop.
+    let mut delay = Delay::new(&clocks);
+
     loop {
-        println!("Hello World!");
         led.toggle().unwrap();
-        delay.delay_ms(1000u32);
+        delay.delay_ms(500u32);
     }
 }
