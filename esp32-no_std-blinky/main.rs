1a24b9b4f47629f3c3208392c81634d4b7454c76
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-04-19 14:55:19 +0200

    feat: ✨ Update esp32 nostd projects

--- esp32-no_std-blinky/main.rs
+++ esp32-no_std-blinky/main.rs
@@ -5,11 +5,12 @@
 #![no_std]
 #![no_main]
 
-use esp32_hal::{
+use esp_backtrace as _;
+use esp_println::println;
+use hal::{
     clock::ClockControl, gpio::IO, peripherals::Peripherals, prelude::*, timer::TimerGroup, Delay,
     Rtc,
 };
-use esp_backtrace as _;
 
 #[entry]
 fn main() -> ! {
@@ -17,17 +18,16 @@ fn main() -> ! {
     let mut system = peripherals.DPORT.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    let timer_group0 = TimerGroup::new(
-        peripherals.TIMG0,
-        &clocks,
-        &mut system.peripheral_clock_control,
-    );
-    let mut wdt = timer_group0.wdt;
+    // Disable the RTC and TIMG watchdog timers
     let mut rtc = Rtc::new(peripherals.RTC_CNTL);
-
-    // Disable MWDT and RWDT (Watchdog) flash boot protection
-    wdt.disable();
+    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    let mut wdt0 = timer_group0.wdt;
+    let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
+    let mut wdt1 = timer_group1.wdt;
     rtc.rwdt.disable();
+    wdt0.disable();
+    wdt1.disable();
+    println!("Hello world!");
 
     // Set GPIO4 as an output, and set its state high initially.
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
--- esp32-no_std-template/main.rs
+++ esp32-no_std-template/main.rs
@@ -1,13 +1,14 @@
 #![no_std]
 #![no_main]
 
+use esp32_hal::{
+    clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc,
+};
 use esp_backtrace as _;
 use esp_println::println;
-use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc};
 
 #[entry]
 fn main() -> ! {
-    init_heap();
     let peripherals = Peripherals::take();
     let system = peripherals.DPORT.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
