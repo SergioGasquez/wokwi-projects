30a0029ca15dda7ce956ec5b24c026e317d43adf
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-04-19 13:47:03 +0200

    ci: 🧪 Test if CI fails when introducing an error

--- esp32-std-template/main.rs
+++ esp32-std-template/main.rs
@@ -1,6 +1,6 @@
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 
 fn main() {
-    esp_idf_sys::link_patches();
-    println!("Hello, world!")
+    esp_idf_sys::link_patches()
+    println!("Hello, world!");
 }
