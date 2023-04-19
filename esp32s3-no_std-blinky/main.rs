ddbd7bc2fec885bf7487669fa9055bd0f205af9c
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-04-19 15:10:13 +0200

    feat: ✨ Update esp32s3 nostd projects

--- esp32s3-no_std-blinky/main.rs
+++ esp32s3-no_std-blinky/main.rs
@@ -1,29 +1,33 @@
 //! Blinks an LED
 //!
 //! This assumes that a LED is connected to the pin assigned to `led`. (GPIO4)
-//
+
 #![no_std]
 #![no_main]
 
-use esp32s3_hal::{
-    clock::ClockControl, gpio::IO, pac::Peripherals, prelude::*, timer::TimerGroup, Delay, Rtc,
-};
 use esp_backtrace as _;
-use xtensa_lx_rt::entry;
+use esp_println::println;
+use hal::{
+    clock::ClockControl, gpio::IO, peripherals::Peripherals, prelude::*, timer::TimerGroup, Delay,
+    Rtc,
+};
 
 #[entry]
 fn main() -> ! {
-    let peripherals = Peripherals::take().unwrap();
-    let system = peripherals.SYSTEM.split();
+    let peripherals = Peripherals::take();
+    let mut system = peripherals.SYSTEM.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
-    let mut wdt = timer_group0.wdt;
+    // Disable the RTC and TIMG watchdog timers
     let mut rtc = Rtc::new(peripherals.RTC_CNTL);
-
-    // Disable MWDT and RWDT (Watchdog) flash boot protection
-    wdt.disable();
+    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    let mut wdt0 = timer_group0.wdt;
+    let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
+    let mut wdt1 = timer_group1.wdt;
     rtc.rwdt.disable();
+    wdt0.disable();
+    wdt1.disable();
+    println!("Hello world!");
 
     // Set GPIO4 as an output, and set its state high initially.
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
@@ -34,10 +38,8 @@ fn main() -> ! {
     // Initialize the Delay peripheral, and use it to toggle the LED state in a
     // loop.
     let mut delay = Delay::new(&clocks);
-    esp_println::println!("Hello, World");
 
     loop {
-        esp_println::println!("Hello, World");
         led.toggle().unwrap();
         delay.delay_ms(500u32);
     }
