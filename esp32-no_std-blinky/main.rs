0b073dd50e10a8e94f92d89f51f5c3c1498acc74
Sergio Gasquez <sergio.gasquez@gmail.com>
2023-04-19 14:25:17 +0200

    feat: ✨ Update esp32-no_std

--- esp32-no_std-blinky/main.rs
+++ esp32-no_std-blinky/main.rs
@@ -1,38 +1,46 @@
+//! Blinks an LED
+//!
+//! This assumes that a LED is connected to the pin assigned to `led`. (GPIO4)
+
 #![no_std]
 #![no_main]
 
 use esp32_hal::{
-    clock::ClockControl, pac::Peripherals, prelude::*, timer::TimerGroup, Delay, Rtc, IO,
+    clock::ClockControl, gpio::IO, peripherals::Peripherals, prelude::*, timer::TimerGroup, Delay,
+    Rtc,
 };
 use esp_backtrace as _;
-use esp_println::println;
-use xtensa_lx_rt::entry;
 
 #[entry]
 fn main() -> ! {
-    let peripherals = Peripherals::take().unwrap();
-    let system = peripherals.DPORT.split();
+    let peripherals = Peripherals::take();
+    let mut system = peripherals.DPORT.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    // Disable the RTC and TIMG watchdog timers
+    let timer_group0 = TimerGroup::new(
+        peripherals.TIMG0,
+        &clocks,
+        &mut system.peripheral_clock_control,
+    );
+    let mut wdt = timer_group0.wdt;
     let mut rtc = Rtc::new(peripherals.RTC_CNTL);
-    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks);
-    let mut wdt0 = timer_group0.wdt;
-    let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
-    let mut wdt1 = timer_group1.wdt;
 
+    // Disable MWDT and RWDT (Watchdog) flash boot protection
+    wdt.disable();
     rtc.rwdt.disable();
-    wdt0.disable();
-    wdt1.disable();
 
-    let mut delay = Delay::new(&clocks);
+    // Set GPIO4 as an output, and set its state high initially.
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
     let mut led = io.pins.gpio4.into_push_pull_output();
+
     led.set_high().unwrap();
 
+    // Initialize the Delay peripheral, and use it to toggle the LED state in a
+    // loop.
+    let mut delay = Delay::new(&clocks);
+
     loop {
-        println!("Hello World!");
         led.toggle().unwrap();
-        delay.delay_ms(1000u32);
+        delay.delay_ms(500u32);
     }
 }
--- esp32-no_std-template/main.rs
+++ esp32-no_std-template/main.rs
@@ -1,13 +1,14 @@
 #![no_std]
 #![no_main]
 
-use esp32_hal::{clock::ClockControl, pac::Peripherals, prelude::*, timer::TimerGroup, Rtc};
 use esp_backtrace as _;
-use xtensa_lx_rt::entry;
+use esp_println::println;
+use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, timer::TimerGroup, Rtc};
 
 #[entry]
 fn main() -> ! {
-    let peripherals = Peripherals::take().unwrap();
+    init_heap();
+    let peripherals = Peripherals::take();
     let system = peripherals.DPORT.split();
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
@@ -17,10 +18,10 @@ fn main() -> ! {
     let mut wdt0 = timer_group0.wdt;
     let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks);
     let mut wdt1 = timer_group1.wdt;
-
     rtc.rwdt.disable();
     wdt0.disable();
     wdt1.disable();
+    println!("Hello world!");
 
     loop {}
 }
